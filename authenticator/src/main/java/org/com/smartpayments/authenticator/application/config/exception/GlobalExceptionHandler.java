1bb3d939e149a3dc7aae61c824e8b4a5d27b9eb7
paiva2 <joaovitor.paiva145@hotmail.com>
2025-11-23 17:57:05 -0300

    refactor/make handleMethodArgumentNotValidException format spring validator messages on request inputs

--- authenticator/src/main/java/org/com/smartpayments/authenticator/application/config/exception/GlobalExceptionHandler.java
+++ authenticator/src/main/java/org/com/smartpayments/authenticator/application/config/exception/GlobalExceptionHandler.java
@@ -3,21 +3,42 @@ package org.com.smartpayments.authenticator.application.config.exception;
 import org.com.smartpayments.authenticator.core.common.exception.base.BadRequestException;
 import org.com.smartpayments.authenticator.core.common.exception.base.ConflictException;
 import org.com.smartpayments.authenticator.core.common.exception.base.NotFoundException;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+
+import static org.springframework.util.ObjectUtils.isEmpty;
+
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, Object>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
-        return new ResponseEntity<>(mapErrors(ex, HttpStatus.BAD_REQUEST.value()), HttpStatus.BAD_REQUEST);
+        HashMap<String, List<String>> errorsMap = new LinkedHashMap<>();
+
+        ex.getAllErrors().forEach(objectError -> {
+            String fieldName = ((DefaultMessageSourceResolvable) objectError.getArguments()[0]).getDefaultMessage();
+
+            if (errorsMap.containsKey(fieldName)) {
+                List<String> currErrorsForField = new ArrayList<>(errorsMap.get(fieldName));
+                currErrorsForField.add(objectError.getDefaultMessage());
+                errorsMap.put(fieldName, currErrorsForField);
+            } else if (!isEmpty(objectError.getDefaultMessage())) {
+                errorsMap.put(fieldName, List.of(objectError.getDefaultMessage()));
+            }
+        });
+
+        return new ResponseEntity<>(mapErrors(ex, errorsMap, HttpStatus.BAD_REQUEST.value()), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(BadRequestException.class)
@@ -33,6 +54,15 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<Map<String, Object>> handleNotFoundException(NotFoundException ex) {
         return new ResponseEntity<>(mapErrors(ex, HttpStatus.NOT_FOUND.value()), HttpStatus.NOT_FOUND);
+    }
+
+    private Map<String, Object> mapErrors(Exception ex, Map<String, List<String>> errors, int statusValue) {
+        return new LinkedHashMap<>() {{
+            put("date", new Date());
+            put("status", statusValue);
+            put("message", errors);
+            put("exception", ex.getClass().getSimpleName());
+        }};
     }
 
     private Map<String, Object> mapErrors(Exception ex, int statusValue) {
